0d3d43b510eb37cd1f6ee41f856f9de8cacc5ba8
Juulia Devadze <jd@odsherred>
2017-11-03 16:31:26 +0100

    Removed Samltest

--- samltest/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../simplesamlphp/lib/_autoload.php');
-$as = new SimpleSAML_Auth_Simple('default-sp');
-$as->requireAuth();
-$attributes = $as->getAttributes();
-print_r($attributes);
-
