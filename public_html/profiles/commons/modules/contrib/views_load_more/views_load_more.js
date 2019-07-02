32166eabf93ed008ea5bd23f0add788eea34a813
juuliabellcom <juuliadevadze@gmail.com>
2019-07-02 18:41:33 +0300

    Views Load More 7.x-1.1->7.x-1.5

--- public_html/profiles/commons/modules/contrib/views_load_more/views_load_more.js
+++ public_html/profiles/commons/modules/contrib/views_load_more/views_load_more.js
@@ -13,7 +13,9 @@
     // our presets.
     var wrapper = response.selector ? $(response.selector) : $(ajax.wrapper);
     var method = response.method || ajax.method;
+    var targetList = response.targetList || '';
     var effect = ajax.getEffect(response);
+    var pager_selector = response.options.pager_selector ? response.options.pager_selector : '.pager-load-more';
 
     // We don't know what response.data contains: it might be a string of text
     // without HTML, so don't rely on jQuery correctly iterpreting
@@ -39,38 +41,99 @@
     // If removing content from the wrapper, detach behaviors first.
     var settings = response.settings || ajax.settings || Drupal.settings;
     Drupal.detachBehaviors(wrapper, settings);
+    if ($.waypoints != undefined) {
+      $.waypoints('refresh');
+    }
+
+    // Set up our default query options. This is for advance users that might
+    // change there views layout classes. This allows them to write there own
+    // jquery selector to replace the content with.
+    // Provide sensible defaults for unordered list, ordered list and table
+    // view styles.
+    var content_query = targetList && !response.options.content ? '> .view-content ' + targetList : response.options.content || '> .view-content';
+
+    // If we're using any effects. Hide the new content before adding it to the DOM.
+    if (effect.showEffect != 'show') {
+      new_content.find(content_query).children().hide();
+    }
+
+    // Update the pager
+    // Find both for the wrapper as the newly loaded content the direct child
+    // .item-list in case of nested pagers
+    wrapper.find(pager_selector).replaceWith(new_content.find(pager_selector));
 
     // Add the new content to the page.
-    wrapper.find('.pager a').remove();
-    wrapper.find('.pager').html(new_content.find('.pager'));
-    wrapper.find('.view-content')[method](new_content.find('.views-row'));
+    wrapper.find(content_query)[method](new_content.find(content_query).children());
+
+    // Re-class the loaded content.
+    // @todo this is faulty in many ways.  first of which is that user may have configured view to not have these classes at all.
+    wrapper.find(content_query).children()
+      .removeClass('views-row-first views-row-last views-row-odd views-row-even')
+      .filter(':first')
+        .addClass('views-row-first')
+        .end()
+      .filter(':last')
+        .addClass('views-row-last')
+        .end()
+      .filter(':even')
+        .addClass('views-row-odd')
+        .end()
+      .filter(':odd')
+        .addClass('views-row-even')
+        .end();
+
+    if (effect.showEffect != 'show') {
+      wrapper.find(content_query).children(':not(:visible)')[effect.showEffect](effect.showSpeed);
+    }
+
+    // Additional processing over new content
+    wrapper.trigger('views_load_more.new_content', new_content.clone());
 
     // Attach all JavaScript behaviors to the new content
-    wrapper.removeClass('views-processed');
-    var settings = response.settings || ajax.settings || Drupal.settings;
+    // Remove the Jquery once Class, TODO: There needs to be a better
+    // way of doing this, look at .removeOnce() :-/
+    var classes = wrapper.attr('class');
+    var onceClass = classes.match(/jquery-once-[0-9]*-[a-z]*/);
+    wrapper.removeClass(onceClass[0]);
+    settings = response.settings || ajax.settings || Drupal.settings;
     Drupal.attachBehaviors(wrapper, settings);
-
-    if (new_content.parents('html').length > 0) {
-      // Apply any settings from the returned JSON if available.
-    }
-  }
+  };
 
   /**
    * Attaches the AJAX behavior to Views Load More waypoint support.
    */
-  Drupal.behaviors.ViewsLoadMore = {};
-  Drupal.behaviors.ViewsLoadMore.attach = function() {
-    if (Drupal.settings && Drupal.settings.viewsLoadMore && Drupal.settings.views.ajaxViews) {
-      opts = {
-        offset: '100%'
-      };
-      $.each(Drupal.settings.viewsLoadMore, function(i, settings) {
-        var view = '.view-' + settings.view_name + '.view-display-id-' + settings.view_display_id + ' .pager-next a';
-        $(view).bind('waypoint.reached', function(event, direction) {
-           $(view).click();
+  Drupal.behaviors.ViewsLoadMore = {
+    attach: function (context, settings) {
+      var default_opts = {
+          offset: '100%'
+        };
+
+      if (settings && settings.viewsLoadMore && settings.views && settings.views.ajaxViews) {
+        $.each(settings.viewsLoadMore, function(i, setting) {
+          var view = '.view-id-' + setting.view_name + '.view-display-id-' + setting.view_display_id + ' .pager-next a',
+            opts = {};
+
+          $.extend(opts, default_opts, settings.viewsLoadMore[i].opts);
+
+          $(view).waypoint('destroy');
+          $(view).waypoint(function(event, direction) {
+            $(view).click();
+          }, opts);
         });
-        $(view).waypoint(opts);
-      });
+      }
+    },
+    detach: function (context, settings, trigger) {
+      if (settings && settings.viewsLoadMore && settings.views && settings.views.ajaxViews) {
+        $.each(settings.viewsLoadMore, function(i, setting) {
+          var view = '.view-id-' + setting.view_name + '.view-display-id-' + setting.view_display_id;
+          if ($(context).is(view)) {
+            $('.pager-next a', view).waypoint('destroy');
+          }
+          else {
+            $(view, context).waypoint('destroy');
+          }
+        });
+      }
     }
   };
 })(jQuery);
